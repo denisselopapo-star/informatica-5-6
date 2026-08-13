8416a9c7ad615914ea9d265bbda50513a03652f0
denisselopapo-star <denisse.lopapo@gmail.com>
2026-08-13 15:40:35 +0000

    POR LO MENOS UN CONEJO SE COMPORTARIA MEJOR QUE TUUU!

--- story.py
+++ story.py
@@ -11,15 +11,21 @@ def main():
     # print("Hello", end=" ")
     # print(planet)
 
-    name = input("What is your name?")
-    color = input("Tell me a color:")
-    goal = input("Tell me a goal:")
-    adjective = input("Give me and adjetive:")
+    name = input("What is your name?").strip().title()
+    color = input("Tell me a color:").strip().lower()
+    goal = input("Tell me a goal:").strip().lower()
+    adjective = input("Give me and adjetive:").strip().lower()
 
     print(f"Hello, {name}!", end="\n\n")
 
     print("this is your story:")
     print(f"At dawn the sky turned {color}, and the air felt {adjective}. I decided today I will finally {goal}.")
+
+    print(f"Hello, {name}!", end="\n\n")
+
+    print("this is your story:")
+    print(f"At dawn the sky turned {color}, and the air felt {adjective}. I decided today I will finally {goal}.".upper())
+
 
 if __name__ =="__main__":
     main()
