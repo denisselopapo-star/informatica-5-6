86b07e46bdc9c7cb5ef4b3a5d980c006fa4164d7
denisselopapo-star <denisse.lopapo@gmail.com>
2026-08-19 18:02:12 +0000

    ¿Con quien se queda el perro?

---
+++ weeklycheckpoint2/update-variable.py
@@ -0,0 +1,41 @@
+def main():
+
+    # Augmented Assignment Operator
+
+    score=10
+    score += 10
+    print(score)
+
+    # Substraction Assignment Operator
+
+    #x = x - y
+    #x -= y
+    score2 = 100
+    score2 -= 15
+    print(score2)
+
+    # Multiplication Assignment Operator
+
+    score3 = 10
+    score3 *=4
+    print(score3)
+
+    # Divition Assignment Operator
+
+    x = 10
+    x /= 5
+    print(int(x))
+
+    # Modulus Assignment Operator
+
+    a = 10
+    b = 4
+    a %= b
+    print(a)
+
+
+
+
+
+if __name__=="__main__":
+    main()
