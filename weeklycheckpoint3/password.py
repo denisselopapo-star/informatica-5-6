3d91963db36973b6eaec86b7e50a12bf096b180c
denisselopapo-star <denisse.lopapo@gmail.com>
2026-08-24 15:01:36 +0000

    I want to go home but its the first period

---
+++ weeklycheckpoint3/password.py
@@ -0,0 +1,12 @@
+def main():
+    password = 987654
+    attempt = int(input("enter your password: "))
+    if attempt == password:
+        print("You successfully knew the password, welcome in! ")
+    print("Have a nice day!")
+
+
+
+if __name__=="__main__":
+    main()
+
