60daa85f0771f39d8fbf8173e4fe2465ccaa0469
denisselopapo-star <denisse.lopapo@gmail.com>
2026-08-28 15:41:29 +0000

    elkr lfdg

--- weeklycheckpoint3/atmosphere.py
+++ weeklycheckpoint3/atmosphere.py
@@ -1,59 +1,73 @@
 def main():
 
-    layer = float(input("Descent atmosphere layer: ")).strip().title()
+    layer = input("Descent atmosphere layer: ").strip().lower()
 
-    Exo = 700, 10000
-    The = 85, 700
-    Mes = 50, 85
-    Str = 12, 50
-    Tro = 0, 12
 
-    if layer == Exosphere:
-        print(f"Your altitude level will be between {Exo} km")
-    elif layer == Thermosphere:
-        print(f"Your altitude level will be between {The} km")
-    elif layer == Mesosphere:
-        print(f"Your altitude level will be between {Mes} km")
-    elif layer == Stratosphere:
-        print(f"Your altitude level will be between {Str} km")
-    elif layer == Troposphere:
-        print(f"Your altitude level will be between {Tro} km")
+    if layer == "Exosphere":
+        print("Your altitude level will be between 700 and 10000 km")
+    elif layer == "Thermosphere":
+        print("Your altitude level will be between 85 and 700 km")
+    elif layer == "Mesosphere":
+        print("Your altitude level will be between 50 and 85 km")
+    elif layer == "Stratosphere":
+        print("Your altitude level will be between 12 and 50 km")
+    elif layer == "Troposphere":
+        print("Your altitude level will be between 0 and 12 km")
     else:
-        print()
+        print("try again")
+
         #operations
-    altitude = int(input(f"Enter exact altitude: "))
-    dis1 =9300+615+35+38+12
-    dis2 =615+35+38+12
-    dis3 =35+38+12
-    dis4 =38+12
-    dis5 =12
-
-    vel1 = 2000+500+200+75+20
-    vel2 = 500+200+75+20
-    vel3 = 200+75+20
-    vel4 = 75+20
-    vel5 = 20
-
-    dis1 /= vel1
-    dis2 /= vel2
-    dis3 /= vel3
-    dis4 /= vel4
-    dis5 /= vel5
+    altitude = float(input("Enter exact altitude: "))
+    time = 0
+    if altitude > 700:
+        time += (altitudee - 700)/2
+        altitude = 700
+    if altitude > 85:
+        time += (altitude-85)/0.5
+        altitude = 85
+    if altitude > 50:
+        time += (altitude-50)/0.2
+        altitude = 50
+    if altitude > 12:
+        time += (altitude-12)/0.075
+        altitude = 12
+    time+=altitude/0.02
+    print("Total time:", round(time,1))
 
 
 
-    if altitude >= 700:
-        print(f"total decent time is {dis1}")
-    elif altitude > 85 :
-        print(f"total decent time is {dis2}")
-    elif altitude > 50 :
-        print(f"total decent time is {dis3}")
-    elif altitude > 12 :
-        print(f"total decent time is {dis4}")
-    elif altitude > 85 :
-        print(f"total decent time is {dis5}")
-    else:
-        print()
+    #dis1 =9300+615+35+38+12
+    #dis2 =615+35+38+12
+    #dis3 =35+38+12
+    #dis4 =38+12
+    #dis5 =12
+
+    #vel1 = 2000+500+200+75+20
+    #vel2 = 500+200+75+20
+    #vel3 = 200+75+20
+    #vel4 = 75+20
+    #vel5 = 20
+
+    #dis1 /= vel1
+    #dis2 /= vel2
+    #dis3 /= vel3
+    #dis4 /= vel4
+    #dis5 /= vel5
+
+
+
+    #if altitude >= 700:
+        #print(f"total decent time is {dis1}")
+    #elif altitude > 85 :
+        #print(f"total decent time is {dis2}")
+    #elif altitude > 50 :
+        #print(f"total decent time is {dis3}")
+    #elif altitude > 12 :
+        #print(f"total decent time is {dis4}")
+    #elif altitude > 85 :
+        #print(f"total decent time is {dis5}")
+    #else:
+        #print()
 
 
 if __name__=="__main__":
--- weeklycheckpoint3/if-exercises.py
+++ weeklycheckpoint3/if-exercises.py
@@ -11,20 +11,20 @@ def main():
         print(Integer)
 
     #Difficulty: Medium - Input Calculator
-    
+
     print("Type your two numbers and the operation of your preference")
-    num1 = float(input("Number 1: "))
-    num2 = float(input("Number 2: "))
+    num1 = int(input("Number 1: "))
+    num2 = int(input("Number 2: "))
     operation = input("Type your operation add, subtract or multiply: ")
     add = num1 + num2
     subtract = num1 - num2
     multiply = num1 * num2
 
-    if operation == add:
+    if operation == "add":
         print(add)
-    elif operation == subtract:
+    elif operation == "subtract":
         print(subtract)
-    elif operation == multiply:
+    elif operation == "multiply":
         print(multiply)
     else:
         print()
