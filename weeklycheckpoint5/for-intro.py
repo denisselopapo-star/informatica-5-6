ebc89819f6ed5cae73be3094af945cc081f8a686
denisselopapo-star <denisse.lopapo@gmail.com>
2026-09-18 20:20:16 +0000

    AMLO

---
+++ weeklycheckpoint5/for-intro.py
@@ -0,0 +1,8 @@
+def main():
+
+    doctrine = ["Faith in Jesus Christ", "Repentance", "Baptism", "Recieving the gift of the holy ghost", "Endure"]
+    for principles in doctrine:
+        print(principles)
+
+if __name__=="__main__":
+    main()
